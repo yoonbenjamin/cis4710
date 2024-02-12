8e574b5cac450de40d231cc2abe6a24f6d5b5100
Ben Yoon <yoonb2002@gmail.com>
2024-02-12 06:33:50 +0000

    hw2a

--- hw1/rca.sv
+++ hw1/rca.sv
@@ -23,7 +23,7 @@ module fulladder(input wire  cin,
                  output wire s,
                  output wire cout);
    wire s_tmp, cout_tmp1, cout_tmp2;
-   halfadder h0(.a(a), .b(b), .s(cout), .cout(cout_tmp1));
+   halfadder h0(.a(a), .b(b), .s(s_tmp), .cout(cout_tmp1));  // corrected output
    halfadder h1(.a(s_tmp), .b(cin), .s(s), .cout(cout_tmp2));
    assign cout = cout_tmp1 | cout_tmp2;
 endmodule
@@ -35,8 +35,10 @@ module fulladder2(input wire        cin,
                   output wire [1:0] s,
                   output wire       cout);
    wire cout_tmp;
-   fulladder a0(.cin(cin), .a(a[0]), .b(b[0]), .s(s[0]), .cout(cout));
-   fulladder a1(.cin(cout_tmp), .a(a[1]), .b(b[1]), .s(s[0]), .cout(cout_tmp));
+   // Corrected connection of fulladder a0 output wires to s[0] and cout_tmp
+   fulladder a0(.cin(cin), .a(a[0]), .b(b[0]), .s(s[0]), .cout(cout_tmp));
+   // Corrected connection of fulladder a1 output wires to s[1] and cout
+   fulladder a1(.cin(cout_tmp), .a(a[1]), .b(b[1]), .s(s[1]), .cout(cout));
 endmodule
 
 /* 4-bit ripple-carry adder that adds two 4-bit numbers (taken from the
@@ -45,7 +47,9 @@ endmodule
 module rca4(input wire  [7:0] SWITCH,
             output wire [7:0] LED);
    wire cout0, ignored;
-   fulladder2 a0(.cin(1'b1), .a(SWITCH[1:0]), .b(SWITCH[5:4]), .s(LED[1:0]), .cout(cout0));
-   fulladder2 a3(.cin(cout0), .a(SWITCH[3:1]), .b(SWITCH[7:6]), .s(LED[3:2]), .cout(ignored));
+   // Corrected fulladder2 a0 input wire cin to be 0
+   fulladder2 a0(.cin(1'b0), .a(SWITCH[1:0]), .b(SWITCH[5:4]), .s(LED[1:0]), .cout(cout0));
+   // Corrected fulladder2 a3 input wire a signal
+   fulladder2 a3(.cin(cout0), .a(SWITCH[3:2]), .b(SWITCH[7:6]), .s(LED[3:2]), .cout(ignored));
    assign LED[7:4] = 4'h0;
 endmodule
--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -1,4 +1,5 @@
 /* INSERT NAME AND PENNKEY HERE */
+/* BENJAMIN YOON / YOONB */
 
 `timescale 1ns / 1ns
 
@@ -12,6 +13,33 @@ module divider_unsigned (
 );
 
     // TODO: your code here
+    wire [31:0] dividend[0:32];
+    wire [31:0] remainder[0:32];
+    wire [31:0] quotient[0:32];
+
+    // Initialize 1st inputs
+    assign dividend[0] = i_dividend;
+    assign remainder[0] = 0;
+    assign quotient[0] = 0;
+
+    genvar i;
+    generate
+        for (i = 0; i < 32; i = i + 1) begin : d_block
+            divu_1iter u_1iter (
+                .i_dividend(dividend[i]),
+                .i_divisor(i_divisor),
+                .i_remainder(remainder[i]),
+                .i_quotient(quotient[i]),
+                .o_dividend(dividend[i + 1]),
+                .o_remainder(remainder[i + 1]),
+                .o_quotient(quotient[i + 1])
+            );
+        end
+    endgenerate
+
+    // Final outputs
+    assign o_remainder = remainder[32];
+    assign o_quotient = quotient[32];
 
 endmodule
 
@@ -39,5 +67,18 @@ module divu_1iter (
     */
 
     // TODO: your code here
+    // Extend to 32 bits
+    wire [31:0] e_bit = {31'b0, i_dividend[31]};
+    wire [31:0] s_remainder = (i_remainder << 1) | e_bit;
+
+    // Shifted remainder greater than or equal to divisor?
+    wire comparison = s_remainder >= i_divisor;
+
+    // Extend to 32 bits & calculate new remainder/quotient
+    assign o_remainder = comparison ? s_remainder - i_divisor : s_remainder;
+    assign o_quotient = (i_quotient << 1) | {31'b0, comparison};
+
+    // Shift dividend left for next iteration
+    assign o_dividend = i_dividend << 1;
 
 endmodule
